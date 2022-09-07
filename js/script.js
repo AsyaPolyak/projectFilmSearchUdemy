3be6e34b0323f25e1e805f5b1d7128cdcaab3780
AsyaPolyak <asyapolyachka@gmail.com>
2022-09-07 14:54:45 +0300

    added task

--- js/script.js
+++ js/script.js
@@ -24,3 +24,31 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img');
+const poster = document.querySelector('.promo__bg');
+const genge = poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
+//adv.remove();
+adv.forEach(item => {
+    item.remove();
+});
+/*
+adv.forEach(function(item) {
+    item.remove();
+});*/
+
+genge.textContent ='Драма';
+
+poster.style.backgroundImage ='url("img/bg.jpg")';
+
+movieList.innerHTML ="";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML +=`
+    <li class="promo__interactive-item">${i + 1}. ${film}
+    <div class="delete"></div>
+</li>
+    `;
+});
